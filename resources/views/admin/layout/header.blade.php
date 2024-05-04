5235fb422df16b19553548c8a2271f6dda232814
MD-FARHAN-UL-ASIF <farhanchowdhury66@gmail.com>
2024-05-04 12:37:22 +0600

    frontend-modified

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -108,7 +108,7 @@
                         </li>
                     </ul>
                     <ul class="nav navbar-nav float-right">
-                        <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link"
+                        {{-- <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link"
                                 id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true"
                                 aria-expanded="false"><i class="flag-icon flag-icon-gb"></i><span
                                     class="selected-language"></span></a>
@@ -119,8 +119,8 @@
                                     href="#" data-language="pt"><i class="flag-icon flag-icon-pt"></i>
                                     Portuguese</a><a class="dropdown-item" href="#" data-language="de"><i
                                         class="flag-icon flag-icon-de"></i> German</a></div>
-                        </li>
-                        <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label"
+                        </li> --}}
+                        {{-- <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label"
                                 href="#" data-toggle="dropdown"><i class="ficon feather icon-bell"></i><span
                                     class="badge badge-pill badge-danger badge-up">5</span></a>
                             <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
@@ -194,8 +194,8 @@
                                 <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"
                                         href="javascript:void(0)">Read all notifications</a></li>
                             </ul>
-                        </li>
-                        <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label"
+                        </li> --}}
+                        {{-- <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label"
                                 href="#" data-toggle="dropdown"><i class="ficon feather icon-mail"></i><span
                                     class="badge badge-pill badge-warning badge-up">3</span></a>
                             <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
@@ -266,7 +266,7 @@
                                 <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"
                                         href="javascript:void(0)">Read all messages</a></li>
                             </ul>
-                        </li>
+                        </li> --}}
                         <li class="dropdown dropdown-user nav-item"><a
                                 class="dropdown-toggle nav-link dropdown-user-link" href="#"
                                 data-toggle="dropdown">
@@ -286,12 +286,12 @@
                                     Edit Profile</a>
                                 <a class="dropdown-item" href="{{ url('admin/update_password') }}"><i
                                         class="feather icon-edit"></i> Update Password</a>
-                                <a class="dropdown-item" href="app-email.html"><i class="feather icon-mail"></i> My
-                                    Inbox</a>
-                                <a class="dropdown-item" href="user-cards.html"><i
+                                {{-- <a class="dropdown-item" href="app-email.html"><i class="feather icon-mail"></i> My
+                                    Inbox</a> --}}
+                                {{-- <a class="dropdown-item" href="user-cards.html"><i
                                         class="feather icon-check-square"></i> Task</a>
                                 <a class="dropdown-item" href="app-chat.html"><i
-                                        class="feather icon-message-square"></i> Chats</a>
+                                        class="feather icon-message-square"></i> Chats</a> --}}
                                 <div class="dropdown-divider"></div><a class="dropdown-item"
                                     href="{{ url('admin/logout') }}"><i class="feather icon-power"></i> Logout</a>
                             </div>
